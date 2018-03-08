a4c550a9fefe87a45a7f86c984039787a33ce5f1
sblack4 <genseb7@gmail.com>
2018-03-08 11:53:21 -0500

    added oracle-cloud specific assets

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+<img src="common/logo-new-v2.svg" align="left" style="height: 6em;"/>
+
+
 # Streaming-Analytics-Demo
 
 Materials to integrate data from multiple sources (Excel, Kafka, Hive, DB) and visualize it in real time using Oracle Cloud. 
@@ -11,11 +14,23 @@ First, let's de-mystify a few acronyms;
 * BDC - [Big Data Cloud](https://cloud.oracle.com/en_US/big-data-cloud)
 * OAC - [Oracle Analytics Cloud](https://cloud.oracle.com/en_US/oac)  
 
+Now this picture should explain the rest: 
+
 ![Notional Architecture](static/notional_architecture.png)
 
 
 ## Getting Started
 
+To get started you should have an Oracle Cloud account with atleast 6 OCPUs.  
+You should follow along in the [Technical Guide](static/Visualizing_Streaming_Data.docx)
+
+Basically do this: 
+1. Stand up Kafka, create a topic
+2. Rename `template-config.py` to `config.py`, fill in config values, and deploy accs-tweet-producer to accs
+3. Stand up BDC, rename `template-config.py` to `config.py`, fill in config values
+4. Submit consumer spark job
+5. Stand up DB & OAC
+6. Connect OAC & BDC
 
 
 ## Thanks & Acknowledgements 
---
+++ bdc-spark/bootstrap-BDC.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# run on BDC 
+# sets up Pandora POC
+#
+# ### FILL IN THESE VAIRABLES ###
+# ### ### BEFORE RUNNING ### ###
+consumer_key = "6vdawFFMZUQXMfGEqVhyOZlEM"
+consumer_secret = "1c2aVGTE0cM2mXxi74tt88H7MgY7NlZXkZTziPRTw564fyqr8l"
+access_token = "13405372-immDs258VJbq5OdhwLVLZioxWn37otbMfdrD3kiWg"
+access_secret = "RBDmGu0Ls2GSHWGa7LXq8nrU1AyRdo1bUzHLnQIArlpzs"
+
+
+#
+### Install my favorite tools ###
+#  clean yum
+echo "cleaning up yum metadata just in case"
+yum clean metadata
+
+# install helper tools
+echo "installing tools"
+yum install -y mlocate git wget vim tree
+
+# update database for locate
+updatedb
+
+#
+### Configure Spark ###
+# TODO: (sblack) 
+# see https://cwiki.apache.org/confluence/display/AMBARI/Modify+configurations 
+# 
+
+
+
+#
+### Install Pyspark Consumer App ### 
+# TODO: (sblack) install pyspark app from git
+# 
+app_home=/home/opc/tweet_consumer
+echo "installing pyspark consumer app in ${app_home}"
+mkdir "${app_home}"
+cat  > "$app_home"/config.py <<EOF
+import logging
+
+consumer_key = $consumer_key
+consumer_secret = $consumer_secret
+access_token = $access_token
+access_secret = $access_secret
+
+track_string = "pandoramusic spotify"
+
+topic = "gse00013079-Pandora"
+kafka_host = "localhost:6667"
+
+log_level = logging.WARN
+
+EOF 
+
+curl -O https://raw.githubusercontent.com/sblack4/spark-streaming-twitter/master/tweet_consumer/tweet_consumer.py
+
+#
+### Start Consumer ###
+# TODO: (sblack) 
